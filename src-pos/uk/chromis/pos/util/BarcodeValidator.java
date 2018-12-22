6118f1c3431643282d7809ad5aa50aee4b7b6474
John <john@chromis.co.uk>
2018-12-22 22:27:48 +0000

    Recoded barcode validator

--- src-pos/uk/chromis/pos/util/BarcodeValidator.java
+++ src-pos/uk/chromis/pos/util/BarcodeValidator.java
@@ -19,8 +19,7 @@
 **    along with Chromis POS.  If not, see <http://www.gnu.org/licenses/>
 **
 **
-*/
-
+ */
 package uk.chromis.pos.util;
 
 /**
@@ -33,116 +32,187 @@ public class BarcodeValidator {
 
     }
 
+    public static String encoderToUPC_E(String barcode) {
+        //Check the length of the barcode UPC-A should be 12 characters
+        if (barcode.length() != 12) {
+            return null;
+        }
+
+        StringBuilder UPC_E = new StringBuilder();
+        StringBuilder key = new StringBuilder();
+        char checkSum = barcode.charAt(11);
+        char numberSystem = barcode.charAt(0);
+        String manufacturer = barcode.substring(1, 6);
+        String product = barcode.substring(6, 11);
+
+        key.append(numberSystem);
+        key.append(checkSum);
+
+        //Only barcodes starting 0 or 1 can be encoded  
+        if (numberSystem != '0' && numberSystem != '1') {
+            return null;
+        }
+
+        switch (manufacturer.substring(2, 5)) {
+            case "000":
+            case "100":
+            case "200":
+                //Check the product code is between 00000 - 00999                    
+                if (!product.startsWith("00")) {
+                    return null;
+                }
+                UPC_E.append("0");
+                UPC_E.append(manufacturer.substring(0, 2));
+                UPC_E.append(product.substring(2, 5));
+                UPC_E.append(manufacturer.substring(2, 3));
+                UPC_E.append(checkSum);
+                return UPC_E.toString();
+            case "300":
+            case "400":
+            case "500":
+            case "600":
+            case "700":
+            case "800":
+            case "900":
+                //Check the product code is between 00000 - 00099 
+                if (!product.startsWith("000")) {
+                    return null;
+                }
+                UPC_E.append("0");
+                UPC_E.append(manufacturer.substring(0, 3));
+                UPC_E.append(product.substring(3, 5));
+                UPC_E.append("3");
+                UPC_E.append(checkSum);
+                return UPC_E.toString();
+        }
+
+        switch (manufacturer.charAt(4)) {
+            case '0':
+                if (!product.startsWith("0000")) {
+                    return null;
+                }
+                UPC_E.append("0");
+                UPC_E.append(manufacturer.substring(0, 4));
+                UPC_E.append(product.charAt(4));
+                UPC_E.append("4");
+                UPC_E.append(checkSum);
+                return UPC_E.toString();
+            default:
+                if (product.charAt(4) >= '0' && product.charAt(4) <= '4') {
+                    return null;
+                }
+                UPC_E.append("0");
+                UPC_E.append(manufacturer.substring(0, 5));
+                UPC_E.append(product.charAt(4));
+                UPC_E.append(checkSum);
+                return UPC_E.toString();
+        }
+
+    }
+
+    public static String encoderToUPC_A(String code) {
+        //UPC-E should always start with 0
+        if (code.charAt(0) != '0') {
+            return null;
+        }
+
+        String barcode = code.substring(1, code.length());
+
+        //Check the length of the barcode UPC-A should be 7 characters
+        if (barcode.length() != 7) {
+            return null;
+        }
+
+        StringBuilder UPC_A = new StringBuilder();
+        char lastChar = barcode.charAt(5);
+
+        switch (lastChar) {
+            case '0':
+            case '1':
+            case '2':
+                UPC_A.append("0");
+                UPC_A.append(barcode.substring(0, 2));
+                UPC_A.append(barcode.charAt(5));
+                UPC_A.append("0000");
+                UPC_A.append(barcode.substring(2, 5));
+                UPC_A.append(barcode.charAt(6));
+                return UPC_A.toString();
+            case '3':
+                UPC_A.append("0");
+                UPC_A.append(barcode.substring(0, 3));
+                UPC_A.append("00000");
+                UPC_A.append(barcode.substring(3, 5));
+                UPC_A.append(barcode.charAt(6));
+                return UPC_A.toString();
+            case '4':
+                UPC_A.append("0");
+                UPC_A.append(barcode.substring(0, 4));
+                UPC_A.append("00000");
+                UPC_A.append(barcode.charAt(4));
+                UPC_A.append(barcode.charAt(6));
+                return UPC_A.toString();
+            default:
+                UPC_A.append("0");
+                UPC_A.append(barcode.substring(0, 5));
+                UPC_A.append("0000");
+                UPC_A.append(barcode.charAt(5));
+                UPC_A.append(barcode.charAt(6));
+                return UPC_A.toString();
+        }
+    }
+
+    private static Boolean validateCheckSum(String barcode) {
+        return validateCheckSum(barcode, false);
+    }
+
+    private static Boolean validateCheckSum(String barcode, Boolean EAN13) {
+        int odd = 0;
+        int even = 0;
+
+        for (int i = 0; i < barcode.length() - 1; i++) {
+            if ((EAN13 ? i : i + 1) % 2 == 0) {
+                even = even + Character.getNumericValue(barcode.charAt(i));
+            } else {
+                odd = odd + (Character.getNumericValue(barcode.charAt(i)) * 3);
+            }
+        }
+
+        int checkDigit = (10 - ((odd + even) % 10)) % 10;
+        if (checkDigit == Character.getNumericValue(barcode.charAt(barcode.length() - 1))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public static String BarcodeValidate(String barcode) {
-
         if (barcode.matches("[0-9]+")) {
-
             int bSize = barcode.length();
-            int odd;
-            int even;
-            int checkDigit;
-            int checkDigit2;
-
             switch (bSize) {
-                case 7:
-                    odd = (Character.getNumericValue(barcode.charAt(0))
-                            + Character.getNumericValue(barcode.charAt(2))
-                            + Character.getNumericValue(barcode.charAt(4))) * 3;
-
-                    even = Character.getNumericValue(barcode.charAt(1))
-                            + Character.getNumericValue(barcode.charAt(3))
-                            + Character.getNumericValue(barcode.charAt(5));
-
-                    checkDigit = (10 - ((odd + even) % 10)) % 10;
-
-                    if (checkDigit == Character.getNumericValue(barcode.charAt(6))) {
+                case 8:
+                    //lets chekc if EAN-8 first
+                    // if that fails test for UPC-E
+                    if (validateCheckSum(barcode)) {
+                        return "EAN-8";
+                    } else if (validateCheckSum(encoderToUPC_A(barcode))) {
                         return "UPC-E";
                     } else {
                         return "null";
                     }
-                case 8:
-                    odd = (Character.getNumericValue(barcode.charAt(0))
-                            + Character.getNumericValue(barcode.charAt(2))
-                            + Character.getNumericValue(barcode.charAt(4))
-                            + Character.getNumericValue(barcode.charAt(6))) * 3;
-
-                    even = Character.getNumericValue(barcode.charAt(1))
-                            + Character.getNumericValue(barcode.charAt(3))
-                            + Character.getNumericValue(barcode.charAt(5));
-
-                    checkDigit = (10 - ((odd + even) % 10)) % 10;
-
-                    if (checkDigit == Character.getNumericValue(barcode.charAt(7))) {
-                        return "EAN-8";
-                    } else {
-                        return "null";
-                    }
-
                 case 12:
-                    odd = (Character.getNumericValue(barcode.charAt(0))
-                            + Character.getNumericValue(barcode.charAt(2))
-                            + Character.getNumericValue(barcode.charAt(4))
-                            + Character.getNumericValue(barcode.charAt(6))
-                            + Character.getNumericValue(barcode.charAt(8))
-                            + Character.getNumericValue(barcode.charAt(10))) * 3;
-
-                    even = Character.getNumericValue(barcode.charAt(1))
-                            + Character.getNumericValue(barcode.charAt(3))
-                            + Character.getNumericValue(barcode.charAt(5))
-                            + Character.getNumericValue(barcode.charAt(7))
-                            + Character.getNumericValue(barcode.charAt(9));
-
-                    checkDigit = (10 - ((odd + even) % 10))% 10;
-
-                    if (checkDigit == Character.getNumericValue(barcode.charAt(11))) {
+                    if (validateCheckSum(barcode)) {
                         return "UPC-A";
                     } else {
                         return "null";
                     }
                 case 13:
-                    odd = (Character.getNumericValue(barcode.charAt(0))
-                            + Character.getNumericValue(barcode.charAt(2))
-                            + Character.getNumericValue(barcode.charAt(4))
-                            + Character.getNumericValue(barcode.charAt(6))
-                            + Character.getNumericValue(barcode.charAt(8))
-                            + Character.getNumericValue(barcode.charAt(10))) * 3;
-
-                    even = Character.getNumericValue(barcode.charAt(1))
-                            + Character.getNumericValue(barcode.charAt(3))
-                            + Character.getNumericValue(barcode.charAt(5))
-                            + Character.getNumericValue(barcode.charAt(7))
-                            + Character.getNumericValue(barcode.charAt(9))
-                            + Character.getNumericValue(barcode.charAt(11));
-
-                    checkDigit = (10 - ((odd + even) % 10)) % 10;
-                    checkDigit2 = (10 - (((odd / 3) + (even * 3)) % 10))% 10;    
-                    
-                    if (checkDigit == Character.getNumericValue(barcode.charAt(12))) {
-                        return "EAN-13";
-                    } else if (checkDigit2 == Character.getNumericValue(barcode.charAt(12))) {
+                    if (validateCheckSum(barcode, true)) {
                         return "EAN-13";
                     } else {
                         return "null";
                     }
                 case 14:
-                    odd = (Character.getNumericValue(barcode.charAt(0))
-                            + Character.getNumericValue(barcode.charAt(2))
-                            + Character.getNumericValue(barcode.charAt(4))
-                            + Character.getNumericValue(barcode.charAt(6))
-                            + Character.getNumericValue(barcode.charAt(8))
-                            + Character.getNumericValue(barcode.charAt(10))
-                            + Character.getNumericValue(barcode.charAt(12))) * 3;
-
-                    even = Character.getNumericValue(barcode.charAt(1))
-                            + Character.getNumericValue(barcode.charAt(3))
-                            + Character.getNumericValue(barcode.charAt(5))
-                            + Character.getNumericValue(barcode.charAt(7))
-                            + Character.getNumericValue(barcode.charAt(9))
-                            + Character.getNumericValue(barcode.charAt(11));
-
-                    checkDigit = (10 - ((odd + even) % 10)) % 10;
-
-                    if (checkDigit == Character.getNumericValue(barcode.charAt(13))) {
+                    if (validateCheckSum(barcode, true)) {
                         return "GTIN";
                     } else {
                         return "null";
@@ -155,3 +225,4 @@ public class BarcodeValidator {
         }
     }
 }
+
